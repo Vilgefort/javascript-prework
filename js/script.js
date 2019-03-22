6926d668e56e2926450e087b7e6dd5cb3ee30ba7
Magda Bula <magdalena.bula@gmail.com>
2019-03-22 09:20:26 +0100

    Develop first part of these part exercise

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var computerMove, playerMove;
+computerMove = "kamień";
+printMessage(
+  "Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz!"
+);
+playerMove = "papier";
+printMessage("papier");
